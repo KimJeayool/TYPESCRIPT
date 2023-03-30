68d72f71aca9e0d6bd0d1d3a779aed66fd7e0950
jaeyoolkim <s09c073@gmail.com>
2023-03-30 21:56:08 +0900

    # string, number, boolean, any, Array, Tuple 타입

--- my-ts/main.ts
+++ my-ts/main.ts
@@ -1,1 +1,26 @@
-console.log(`Hello World at console`); 
+// console.log(`Hello World at console`); 
+
+/////////////////////////////////////////////////////////
+// Type 이란?
+/////////////////////////////////////////////////////////
+let myName: string = 'chestNut';
+// myName = 35;
+
+let myAge: number = 35;
+// myAge = '35';
+
+let isHeCome: boolean = false;
+// isHeCome = 'chestNut';
+
+let hisBag: any;  // 타입 미할당
+hisBag = 35;
+hisBag = "Name";
+
+let heroes: string[] = ['Superman', 'Hulk', 'Batman'];
+// heroes = 50;
+heroes = ["50"];
+
+// Tuple
+let classB: [string, number] = ["Students", 20];
+// classB = ["Teacher"];
+classB = ["Teacher", 2];
